f2d2ad481ce183ab37fcff3543e914e97b8bef0c
Daniel F. Dickinson <dfdpublic@wildtechgarden.ca>
2022-04-04 02:19:35 -0400

    Fix shfmt lint errors
    Signed-off-by: Daniel F. Dickinson <dfdpublic@wildtechgarden.ca>

--- bash/html-validate-minified.sh
+++ bash/html-validate-minified.sh
@@ -2,8 +2,7 @@
 
 set -o pipefail
 
-if npx html-validate --config .htmlvalidate-minified.json public 2>&1 | tee html-validate.log
-then
+if npx html-validate --config .htmlvalidate-minified.json public 2>&1 | tee html-validate.log; then
     echo "ok"
 else
     echo "not ok"
--- bash/hugo-audit-minified.sh
+++ bash/hugo-audit-minified.sh
@@ -7,7 +7,8 @@ set -o pipefail
 if HUGO_MINIFY_TDEWOLFF_HTML_KEEPCOMMENTS=true HUGO_ENABLEMISSINGTRANSLATIONPLACEHOLDERS=true "HUGO_RESOURCEDIR=$(pwd)/resources" "$HUGO_COMMAND" --minify --gc --cleanDestinationDir --destination "$(pwd)/public" --source "$(pwd)"; then
     # If hugo build succeeds but possible audit issues are present, check further
     # Check for problem indicators (see https://discourse.gohugo.io/t/audit-your-published-site-for-problems/35184)
-    grep -iIrnE '<\!-- raw HTML omitted -->|ZgotmplZ|hahahugo|\[i18n\]|\(<nil>\)|\(&lt;nil&gt;\)' public/ >hugo-audit.log; RET=$?
+    grep -iIrnE '<\!-- raw HTML omitted -->|ZgotmplZ|hahahugo|\[i18n\]|\(<nil>\)|\(&lt;nil&gt;\)' public/ >hugo-audit.log
+    RET=$?
     if [ "$RET" != "1" ] && [ "$RET" != "0" ]; then
         # Make sure we fail if there is is error executing the check command
         echo "not ok"
@@ -17,7 +18,8 @@ if HUGO_MINIFY_TDEWOLFF_HTML_KEEPCOMMENTS=true HUGO_ENABLEMISSINGTRANSLATIONPLAC
     # Check if problem indicators are part of some page's content (e.g. a page describing how
     # to check for Hugo audit issues).
     if [ "$RET" = "0" ]; then
-        grep -iIvE 'grep(.+(-- raw HTML omitted --|ZgotmplZ|hahahugo|\[i18n\]|\(<nil>\)|\(&lt;nil&gt;\))+)' hugo-audit.log; RET2=$?
+        grep -iIvE 'grep(.+(-- raw HTML omitted --|ZgotmplZ|hahahugo|\[i18n\]|\(<nil>\)|\(&lt;nil&gt;\))+)' hugo-audit.log
+        RET2=$?
         if [ "$RET2" != "1" ]; then
             # Make sure we fail if there is an error executing the false positive elimination command (exit code 2)
             # Also fail if there was a true positive (exit code 0)
