4ec631f8207c92a8c92b51e9f03ecf8a693f86c1
Daniel F. Dickinson <dfdpublic@wildtechgarden.ca>
2022-04-04 13:46:45 -0400

    Genericize check-*-links.sh
    Signed-off-by: Daniel F. Dickinson <dfdpublic@wildtechgarden.ca>

--- bash/check-external-links.sh
+++ bash/check-external-links.sh
@@ -1,15 +1,46 @@
 #!/bin/bash
 
-npx hyperlink public/sitemap.xml --canonicalroot https://www.wildtechgarden.ca/ \
-    --skip "https://www.facebook.com/sharer.php" \
-    --skip "https://linkedin.com/shareArticle" \
-    --skip "https://twitter.com/share" \
-    --skip "https://www.pexels.com" \
-    --skip "https://pixabay.com" \
-    --skip "www.facebook.com" \
-    --skip "business.facebook.com" \
-    --skip "example.com" \
-    --skip "example.net" \
+if [ -z "$SKIP_CHECK_PATTERNS" ]; then
+    SKIP_CHECK_PATTERNS="https://www.facebook.com/sharer.php \
+https://www.linkedin.com/shareArticle  \
+https://linkedin.com/shareArticle \
+https://twitter.com/share \
+https://www.pexels.com \
+https://pixabay.com \
+http://doi.org/ \
+https://doi.org/ \
+https://www.nature.com/ \
+https://arxiv.org/ \
+https://www.facebook.com/ \
+https://facebook.com/ \
+https://business.facebook.com \
+example.com \
+example.net"
+fi
+
+CROOT="${1:-https://www.example.com}"
+SITEROOT="."
+
+if [ -d exampleSite ]; then
+    SITEROOT=exampleSite
+fi
+
+if grep -q baseURL "${SITEROOT}"/config.toml; then
+    CROOT="$(grep baseURL exampleSite/config.toml | sed -e 's/^.*[^=]* *'\''\([^'\'']*\)'\'' *$/\1/')"
+fi
+
+echo "Using ${CROOT} as canonicalroot"
+
+SKIPS=""; for skip in ${SKIP_CHECK_PATTERNS}; do SKIPS="${SKIPS}${SKIPS:+ }--skip \"${skip}\""; done ; \
+
+URLBASE=public/sitemap.xml
+
+if [ ! -e "$URLBASE" ]; then
+    URLBASE=public/index.html
+fi
+
+npx hyperlink "$URLBASE" --canonicalroot "${CROOT}" \
+    ${SKIPS} \
     --todo "301 http" \
     --todo "fragment-redirect" \
-    -r -p public/sitemap.xml | tee check-links.log | npx tap-spot
+    -r -p "$URLBASE" | tee check-links.log | npx tap-spot
